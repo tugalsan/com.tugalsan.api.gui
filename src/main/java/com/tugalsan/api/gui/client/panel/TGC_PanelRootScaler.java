31932adc4e371d8e37430bb53165b4981466a352
tugalsan karabacak <tugalsan@gmail.com>
2023-02-10 13:27:44 +0300

    .

---
+++ src/main/java/com/tugalsan/api/gui/client/panel/TGC_PanelRootScaler.java
@@ -0,0 +1,33 @@
+package com.tugalsan.api.gui.client.panel;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.tugalsan.api.gui.client.browser.TGC_BrowserNavigatorUtils;
+
+public class TGC_PanelRootScaler {
+
+    public TGC_PanelRootScaler(int widthDefault) {
+        this.widthDefault = widthDefault;
+    }
+    public int widthDefault;
+
+    private double scale(int currentWidth) {
+        return Math.min(currentWidth, widthDefault) / (double) widthDefault;
+    }
+
+    private void setProperty(String name, String value) {
+        RootPanel.get().getElement().getStyle().setProperty(name, value);
+    }
+
+    public void zoomBody(int currentWidth) {
+        var scale = scale(currentWidth);
+        if (TGC_BrowserNavigatorUtils.firefox()) {
+            setProperty("transform", "scale(" + scale + ")"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            setProperty("marginLeft", 50 - 50 / scale + "%"); //$NON-NLS-1$
+            setProperty("marginTop", Window.getClientHeight() * (scale - 1) / 2 + "px");
+            setProperty("width", 100 / scale + "%");
+            return;
+        }
+        setProperty("zoom", String.valueOf(scale(currentWidth)));
+    }
+}
