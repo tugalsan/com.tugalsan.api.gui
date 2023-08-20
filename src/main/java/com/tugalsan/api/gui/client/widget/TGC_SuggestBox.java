28abd89beecfbcb09d653ff4689702f2dd2ca49b
tugalsan karabacak <tugalsan@gmail.com>
2023-08-20 11:46:05 +0300

    Update TGC_SuggestBox.java

--- src/main/java/com/tugalsan/api/gui/client/widget/TGC_SuggestBox.java
+++ src/main/java/com/tugalsan/api/gui/client/widget/TGC_SuggestBox.java
@@ -1,24 +1,26 @@
 package com.tugalsan.api.gui.client.widget;
 
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.SuggestBox;
-import com.google.gwt.user.client.ui.SuggestOracle;
 import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
 import com.google.gwt.user.client.ui.TextBox;
-import com.tugalsan.api.stream.client.TGS_StreamUtils;
 import java.util.Collection;
 import java.util.List;
 
 public class TGC_SuggestBox extends SuggestBox {
 
     public TGC_SuggestBox(List<String> items) {
-        super(new SuggestOracle() {
-            @Override
-            public void requestSuggestions(Request request, Callback callback) {
-                var response = new Response();
-                response.setSuggestions(TGS_StreamUtils.toLst(items.stream().map(item -> new TGC_SuggestBoxSuggestion(item))));
-                callback.onSuggestionsReady(request, response);
-            }
-        }, new TextBox(), new DefaultSuggestionDisplay() {
+        super(
+                //        new SuggestOracle() {
+                //            @Override
+                //            public void requestSuggestions(Request request, Callback callback) {
+                //                var response = new Response();
+                //                response.setSuggestions(TGS_StreamUtils.toLst(items.stream().map(item -> new TGC_SuggestBoxSuggestion(item))));
+                //                callback.onSuggestionsReady(request, response);
+                //            }
+                //        }
+                new MultiWordSuggestOracle(),
+                new TextBox(), new DefaultSuggestionDisplay() {
             @Override
             protected void showSuggestions(SuggestBox suggestBox, Collection<? extends Suggestion> suggestions, boolean isDisplayStringHTML, boolean isAutoSelectEnabled, SuggestionCallback callback) {
                 if (suggestBox.getText().length() > 1) {
@@ -28,22 +30,30 @@ public class TGC_SuggestBox extends SuggestBox {
         });
     }
 
-    private static class TGC_SuggestBoxSuggestion implements Suggestion {
-
-        private final String text;
-
-        public TGC_SuggestBoxSuggestion(String text) {
-            this.text = text;
-        }
-
-        @Override
-        public String getDisplayString() {
-            return text;
-        }
-
-        @Override
-        public String getReplacementString() {
-            return text;
-        }
+    public void addSuggestion(Collection<String> items) {
+        ((MultiWordSuggestOracle) getSuggestOracle()).addAll(items);
     }
+
+    public void addSuggestion(String item) {
+        ((MultiWordSuggestOracle) getSuggestOracle()).add(item);
+    }
+
+//    private static class TGC_SuggestBoxSuggestion implements Suggestion {
+//
+//        private final String text;
+//
+//        public TGC_SuggestBoxSuggestion(String text) {
+//            this.text = text;
+//        }
+//
+//        @Override
+//        public String getDisplayString() {
+//            return text;
+//        }
+//
+//        @Override
+//        public String getReplacementString() {
+//            return text;
+//        }
+//    }
 }
