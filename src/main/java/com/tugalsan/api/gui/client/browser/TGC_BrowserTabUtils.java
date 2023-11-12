525ea6c7e977971c674a520435bb6f608a924b8a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-11-12 13:23:38 +0300

    .

--- src/main/java/com/tugalsan/api/gui/client/browser/TGC_BrowserTabUtils.java
+++ src/main/java/com/tugalsan/api/gui/client/browser/TGC_BrowserTabUtils.java
@@ -1,27 +1,26 @@
 package com.tugalsan.api.gui.client.browser;
 
-import com.tugalsan.api.runnable.client.TGS_RunnableType1;
 import com.tugalsan.api.log.client.TGC_Log;
-import elemental2.dom.BroadcastChannel;
-import elemental2.dom.MessageEvent;
+//import elemental2.dom.BroadcastChannel;
+//import elemental2.dom.MessageEvent;
 
-@Deprecated //TODO BroadCast NOT WORKING
+@Deprecated //TODO BroadCast NOT WORKING, no need add another dep for it
 public class TGC_BrowserTabUtils {
 
     final private static TGC_Log d = TGC_Log.of(TGC_BrowserTabUtils.class);
 
-    public static BroadcastChannel addBroadCastChannel(CharSequence channelName, TGS_RunnableType1<MessageEvent<Object>> exe) {
-        var bc = new BroadcastChannel(channelName.toString());
-        d.cr("onModuleLoad", "bc.name", bc.name);
-        bc.onmessage = (MessageEvent<Object> e) -> {
-            var msg = String.valueOf(e.data);
-            d.cr("addBroadCastChannel.onmessage.onInvoke", "msg", msg);
-            return msg;
-        };
-        return bc;
-    }
-
-    public static Object broadCast(BroadcastChannel bc, Object msg) {
-        return bc.postMessage(msg);
-    }
+//    public static BroadcastChannel addBroadCastChannel(CharSequence channelName, TGS_RunnableType1<MessageEvent<Object>> exe) {
+//        var bc = new BroadcastChannel(channelName.toString());
+//        d.cr("onModuleLoad", "bc.name", bc.name);
+//        bc.onmessage = (MessageEvent<Object> e) -> {
+//            var msg = String.valueOf(e.data);
+//            d.cr("addBroadCastChannel.onmessage.onInvoke", "msg", msg);
+//            return msg;
+//        };
+//        return bc;
+//    }
+//
+//    public static Object broadCast(BroadcastChannel bc, Object msg) {
+//        return bc.postMessage(msg);
+//    }
 }
