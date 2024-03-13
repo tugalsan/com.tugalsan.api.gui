ab3381a538fd8a71e1e0fd14b8f02bff15f2ea94
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-03-13 09:30:21 +0300

    Update TGC_BrowserFavIconUtils.java

--- src/main/java/com/tugalsan/api/gui/client/browser/TGC_BrowserFavIconUtils.java
+++ src/main/java/com/tugalsan/api/gui/client/browser/TGC_BrowserFavIconUtils.java
@@ -5,4 +5,8 @@ public class TGC_BrowserFavIconUtils {
     public static native void set(CharSequence href) /*-{
         $wnd.setFavicon(href);
     }-*/;
+    
+    public static native void setText(CharSequence text) /*-{
+        $wnd.setFavicon(href);
+    }-*/;
 }
