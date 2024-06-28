10b2b4c083d776aec43a3f6627acdef7eb15f34b
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-28 10:18:37 +0300

    Update TGC_DateBoxUtils.java

--- src/main/java/com/tugalsan/api/gui/client/widget/TGC_DateBoxUtils.java
+++ src/main/java/com/tugalsan/api/gui/client/widget/TGC_DateBoxUtils.java
@@ -24,6 +24,6 @@ public class TGC_DateBoxUtils {
     }
 
     public static TGS_Time getDate(DateBox w) {
-        return TGS_Time.ofDate(w.getTextBox().getText());
+        return TGS_Time.ofDate_D_M_Y(w.getTextBox().getText());
     }
 }
