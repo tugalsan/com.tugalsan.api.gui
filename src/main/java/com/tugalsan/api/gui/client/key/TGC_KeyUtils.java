afe1621e4d96e613f202df45b5a7969700777d44
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-08-28 14:26:26 +0300

    Update TGC_KeyUtils.java

--- src/main/java/com/tugalsan/api/gui/client/key/TGC_KeyUtils.java
+++ src/main/java/com/tugalsan/api/gui/client/key/TGC_KeyUtils.java
@@ -10,6 +10,7 @@ import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.ValueBoxBase;
 import com.tugalsan.api.function.client.TGS_Func;
 
 import com.tugalsan.api.list.client.*;
@@ -17,7 +18,6 @@ import com.tugalsan.api.list.client.*;
 public class TGC_KeyUtils {
 
 //    final private static TGC_Log d = TGC_Log.of(TGC_KeyUtils.class);
-
     public static void addEsc(TGS_Func exe, FocusWidget... w) {
         TGS_ListUtils.of(w).forEach(wi -> wi.addKeyDownHandler(e -> {
             if (e.isAnyModifierKeyDown()) {
@@ -114,8 +114,8 @@ public class TGC_KeyUtils {
         });
     }
 
-    public static void add(TextBox btn, TGS_Func enter, TGS_Func esc) {
-        btn.addKeyDownHandler(e -> {
+    public static void add(ValueBoxBase tb, TGS_Func enter, TGS_Func esc) {
+        tb.addKeyDownHandler(e -> {
             if (e.isAnyModifierKeyDown()) {
                 return;
             }
@@ -137,8 +137,31 @@ public class TGC_KeyUtils {
         });
     }
 
-    public static void add(ListBox btn, TGS_Func enter, TGS_Func esc) {
-        btn.addKeyDownHandler(e -> {
+    public static void add(TextBox tb, TGS_Func enter, TGS_Func esc) {
+        tb.addKeyDownHandler(e -> {
+            if (e.isAnyModifierKeyDown()) {
+                return;
+            }
+            var b = false;
+            if (e.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+                if (enter != null) {
+                    enter.run();
+                    b = true;
+                }
+            } else if (e.getNativeKeyCode() == KeyCodes.KEY_ESCAPE) {
+                if (esc != null) {
+                    esc.run();
+                    b = true;
+                }
+            }
+            if (b) {
+                e.preventDefault();
+            }
+        });
+    }
+
+    public static void add(ListBox lb, TGS_Func enter, TGS_Func esc) {
+        lb.addKeyDownHandler(e -> {
             if (e.isAnyModifierKeyDown()) {
                 return;
             }
@@ -160,14 +183,14 @@ public class TGC_KeyUtils {
         });
     }
 
-    public static void add(CheckBox btn, TGS_Func enter, TGS_Func esc) {
-        btn.addKeyDownHandler(e -> {
+    public static void add(CheckBox cb, TGS_Func enter, TGS_Func esc) {
+        cb.addKeyDownHandler(e -> {
             if (e.isControlKeyDown() || e.isShiftKeyDown() || e.isAltKeyDown()) {
                 return;
             }
             switch (e.getNativeKeyCode()) {
                 case KeyCodes.KEY_ENTER:
-                    btn.setValue(!btn.getValue());//FIX
+                    cb.setValue(!cb.getValue());//FIX
                     if (enter != null) {
                         enter.run();
                     }
@@ -188,12 +211,12 @@ public class TGC_KeyUtils {
         });
     }
 
-    public static void add(RadioButton btn, TGS_Func enter, TGS_Func esc) {
-        add((CheckBox) btn, enter, esc);
+    public static void add(RadioButton rb, TGS_Func enter, TGS_Func esc) {
+        add((CheckBox) rb, enter, esc);
     }
 
-    public static void add(TextArea btn, TGS_Func controlEnter, TGS_Func esc) {
-        btn.addKeyDownHandler(e -> {
+    public static void add(TextArea ta, TGS_Func controlEnter, TGS_Func esc) {
+        ta.addKeyDownHandler(e -> {
             var b = false;
             if (e.getNativeKeyCode() == KeyCodes.KEY_ESCAPE) {
                 if (e.isAnyModifierKeyDown()) {
