81263cf3256879e22de235a5a4ff1abcf0c4c4f8
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-12-09 09:08:14 +0300

    Update TGC_PanelStyleUtils.java

--- src/main/java/com/tugalsan/api/gui/client/theme/TGC_PanelStyleUtils.java
+++ src/main/java/com/tugalsan/api/gui/client/theme/TGC_PanelStyleUtils.java
@@ -33,10 +33,24 @@ public class TGC_PanelStyleUtils {
         });
     }
 
+    public static void blackLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelBlack.class.getSimpleName());
+        });
+    }
+
     public static void yellow(Widget... widgets) {
         remove(widgets);
         Arrays.stream(widgets).forEachOrdered(w -> {
             w.setStyleName(TGC_PanelYellow.class.getSimpleName());
+        });
+    }
+
+    public static void yellowLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelYellow.class.getSimpleName());
         });
     }
 
@@ -47,10 +61,24 @@ public class TGC_PanelStyleUtils {
         });
     }
 
+    public static void redLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelRed.class.getSimpleName());
+        });
+    }
+
     public static void green(Widget... widgets) {
         remove(widgets);
         Arrays.stream(widgets).forEachOrdered(w -> {
             w.setStyleName(TGC_PanelGreen.class.getSimpleName());
+        });
+    }
+
+    public static void greenLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelGreen.class.getSimpleName());
         });
     }
 
@@ -61,10 +89,24 @@ public class TGC_PanelStyleUtils {
         });
     }
 
+    public static void blueLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelBlue.class.getSimpleName());
+        });
+    }
+
     public static void gray(Widget... widgets) {
         remove(widgets);
         Arrays.stream(widgets).forEachOrdered(w -> {
             w.setStyleName(TGC_PanelGray.class.getSimpleName());
+        });
+    }
+
+    public static void grayLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelGray.class.getSimpleName());
         });
     }
 
@@ -75,10 +117,24 @@ public class TGC_PanelStyleUtils {
         });
     }
 
+    public static void purpleLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelPurple.class.getSimpleName());
+        });
+    }
+
     public static void orange(Widget... widgets) {
         remove(widgets);
         Arrays.stream(widgets).forEachOrdered(w -> {
             w.setStyleName(TGC_PanelOrange.class.getSimpleName());
+        });
+    }
+
+    public static void orangeLoosely(Widget... widgets) {
+        remove(widgets);
+        Arrays.stream(widgets).forEachOrdered(w -> {
+            w.addStyleName(TGC_PanelOrange.class.getSimpleName());
         });
     }
 }
