b21c3a982f313d4745eb80cb73defa725b28075a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-12-11 06:10:05 +0300

    Update TGC_BrowserTabUtils.java

--- src/main/java/com/tugalsan/api/gui/client/browser/TGC_BrowserTabUtils.java
+++ src/main/java/com/tugalsan/api/gui/client/browser/TGC_BrowserTabUtils.java
@@ -1,11 +1,23 @@
 package com.tugalsan.api.gui.client.browser;
 //import elemental2.dom.MessageEvent;
 
-@Deprecated //TODO BroadCast NOT WORKING, no need add another dep for it
+
 public class TGC_BrowserTabUtils {
 
-//    final private static TGC_Log d = TGC_Log.of(TGC_BrowserTabUtils.class);
+    public static native void addEventListener_visibilitychange() /*-{
+        $doc.addEventListener("visibilitychange", () => {
+            var tabIsVisible = !document.hidden
+            if (document.hidden) {
+                tabIsVisible = false;
+            } else {
+              tabIsVisible = true;
+            }
+            console.log("tabIsVisible:" + tabIsVisible)
+        });   
+    }-*/;
 
+//@Deprecated //TODO BroadCast NOT WORKING, no need add another dep for it
+//    final private static TGC_Log d = TGC_Log.of(TGC_BrowserTabUtils.class);
 //    public static BroadcastChannel addBroadCastChannel(CharSequence channelName, TGS_Func_In1<MessageEvent<Object>> exe) {
 //        var bc = new BroadcastChannel(channelName.toString());
 //        d.cr("onModuleLoad", "bc.name", bc.name);
