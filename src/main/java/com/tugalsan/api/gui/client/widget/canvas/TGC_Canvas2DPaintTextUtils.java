da31d8ffd8984e56536c669c8069d21a6556ec2c
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-01-21 12:31:19 +0300

    Update TGC_Canvas2DPaintTextUtils.java

--- src/main/java/com/tugalsan/api/gui/client/widget/canvas/TGC_Canvas2DPaintTextUtils.java
+++ src/main/java/com/tugalsan/api/gui/client/widget/canvas/TGC_Canvas2DPaintTextUtils.java
@@ -6,7 +6,14 @@ import com.tugalsan.api.shape.client.TGS_ShapeLocation;
 public class TGC_Canvas2DPaintTextUtils {
 
     public static Canvas paint(Canvas canvas, TGS_ShapeLocation<Integer> loc, CharSequence text) {
+        return paint(canvas, loc, text, null);
+    }
+
+    public static Canvas paint(Canvas canvas, TGS_ShapeLocation<Integer> loc, CharSequence text, CharSequence font) {
         var c2d = TGC_Canvas2DUtils.toContext2d(canvas);
+        if (font != null) {
+            c2d.setFont(font.toString());
+        }
         c2d.fillText(text.toString(), loc.x, loc.y);
         return canvas;
     }
